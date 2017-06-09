278a1b31cee83025987c74203889f7067173c234
Norbert <norbert_2000@o2.pl>
2017-06-09 23:34:00 +0200

    completed program

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@
 int main()
 {
 
-	std::cout << "bmi calculator" << std::endl;
+	double mass;
+	double growth;
+	double bmi;
+
+	std::cout << "Enter your mass: " << std::endl;
+	std::cin >> mass;
+	std::cout << "Enter your growth: " << std::endl;
+	std::cin >> growth;
+
+	bmi = mass / (growth * growth); //to designate bmi
+	bmi *= 10000; //to show e.g. 22.50 not 0.0002250
+	std::cout.precision(4); //to show e.g. 22.50 not 22.50323
+
+	std::cout << "Your BMI is : " << bmi << std::endl;
+
 
 	getch();
 
